3f21083e9579e76fb464f1ad625238d732630349
albycc <78078184+albycc@users.noreply.github.com>
2022-04-10 14:42:40 +0200

    user can loggin in loginpage
    login validation for empty fields or unknown emial or password

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -29,4 +29,4 @@ function loginTestUser(){
 
 };
 
-loginTestUser();
+// loginTestUser();
---
+++ frontend/pages/logincreateaccount/js/login.js
@@ -0,0 +1,40 @@
+
+const emailField = $('#email-input-field');
+const passwordField = $('#password-input-field');
+const errorMessage = $('#error-message');
+
+$('#login-btn').on('click', (e)=>{
+    e.preventDefault();
+
+    if(emailField.val() == "" || passwordField.val() == ""){
+        console.log(errorMessage.parent().hasClass('hide-visible'))
+        printErrorMessage("Empty fields");
+        return;
+    }
+
+    console.log('login function call')
+    axios.post('http://localhost:1337/api/auth/local', {
+        identifier: emailField.val(),
+        password: passwordField.val(),
+    })
+    .then(response =>{
+        console.log('Login success');
+        const {data} = response;
+        sessionStorage.setItem('userData', JSON.stringify(data))
+        console.log('Login details: ', data);
+        location.href= '../../index.html';
+
+    })
+    .catch(error =>{
+        if(error.response.status == 400){
+            printErrorMessage('Wrong email or password')
+        }
+    })
+})
+
+function printErrorMessage(message){
+    if(errorMessage.parent().hasClass('hide-visible')){
+        errorMessage.parent().removeClass('hide-visible');
+    }
+    errorMessage.text(message)
+}
