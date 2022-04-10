f9e91f24cd92f321db4ee48837d9ab542682d090
albycc <78078184+albycc@users.noreply.github.com>
2022-04-10 10:34:26 +0200

    new way of creating book types
    Instead of creating with type component, books and audiobooks are created with collection types instead.
    
    New Audiobook collection type
    
    added nr of pages attribute for Book collection type

---
+++ backend/src/api/book-collection/controllers/book-collection.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  book-collection controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::book-collection.book-collection');
