e5140775f7b3f221564b6e385f6f40a66d272e9a
albycc <78078184+albycc@users.noreply.github.com>
2022-04-12 11:16:57 +0200

    Created a tab group for users loaned and registered books in profile page
    Made with pure css

--- frontend/pages/userprofile/js/profilescript.js
+++ frontend/pages/userprofile/js/profilescript.js
@@ -1,13 +1,10 @@
 import {userData} from '../../../js/main.js'
 
-const booksLoanedCollection = $('#books-loaned-collection');
+const booksLoanedContent = $('#book-collection-books-loaned');
 
-const loanTab = $('#loan-tab');
-const registeredTab = $('#registered-tab')
 
 async function fetchUserInfo(){
 
-    console.log(userData)
 
     const {data:{data}} = await axios.get(`http://localhost:1337/api/books?populate=*&filters[loanedBy][id][$eq]=${userData.user.id}`, {
         headers:{
@@ -15,23 +12,22 @@ async function fetchUserInfo(){
         },
     });
 
-    console.log(data);
-
     $('h1').text(userData.user.username);
 
     const {id, email, createdAt} = userData.user;
     const userList = [id, email, createdAt]
 
     $('.user-info-row > span + span').each((i,e) =>{
-        console.log(e)
         e.textContent = userList[i]
     })
 
-    loanTab.children('span.book-tab-count').text(data.length)
+    $('input#loaned-tab + label > span.book-tab-count').text(data.length)
+
+    console.log($('input#loaned-tab ~ label > span.book-tab-count'))
 
     data.forEach(book => {
         console.log(book.attributes.cover)
-        $('#books-loaned-collection').append(`
+        booksLoanedContent.append(`
             <div class="book">
                 <img src="http://localhost:1337${book.attributes.cover.data.attributes.url}"></img>
             </div>
