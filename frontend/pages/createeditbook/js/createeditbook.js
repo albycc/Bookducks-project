ac7dbcdba79c6893c30abe14985c61e830ca0ab2
albycc <78078184+albycc@users.noreply.github.com>
2022-04-13 14:10:02 +0200

    can upload cover images to backend
    implemented multipleValue selection.
    
    genre, authors and narratedBy fields now have multipleValue set

--- frontend/pages/createeditbook/js/createeditbook.js
+++ frontend/pages/createeditbook/js/createeditbook.js
@@ -1,8 +1,15 @@
 // createeditbook.js
 import {userData} from '../../../js/main.js'
 
-const radioBooktypeGrp = $('[name="type-radio-grp"]');
 let radioValue;
+
+const genreValues = $('#genres-values')
+const authorsValues = $('#authors-values')
+const authorInputfield = $('#input-field-authors')
+
+const narrationsValues = $('#narrations-values')
+const narrationsInputfield = $('#input-field-narrators')
+
 
 $('[name="type-radio-grp"]').on('click', toggleRadioButtons)
 
@@ -24,10 +31,10 @@ $('#submit-btn').on('click', async (e)=>{
         console.log(required)
         return;
     }
+
     
     const inputs = $('#form-input-list > li').not('[style="display: none;"]').find('[data-input]');
-
-    console.log(inputs)
+    const multipleValues = $('#form-input-list > li').not('[style="display: none;"]').find('[data-multiple-prop]')
 
     const bookObject = {}
     inputs.each((i,e) =>{
@@ -41,16 +48,47 @@ $('#submit-btn').on('click', async (e)=>{
         bookObject[`${e.dataset.input}`] = value
     })
 
+    multipleValues.each((i,e) =>{
+        const nameArray = [];
+        const children = Array.prototype.slice.call(e.children)
+        children.forEach(option =>{
+            nameArray.push(option.textContent)
+        })
+        bookObject[`${e.dataset.multipleProp}`] = nameArray.join(', ');
+    })
+
+    //get genres
+    const genreArray = genreValues.children().toArray().map(option => option.value)
+    bookObject.genres = genreArray
+
+    //get cover image
+    const image = $('#cover-input')[0].files[0];
+    const imgData = new FormData();
+    console.log(image)
+    imgData.append('files', image)
+
     console.log(bookObject);
 
-    await axios.post(`http://localhost:1337/api/${radioValue}`, {
-        data:bookObject
-    },
-    {
+    await axios.post('http://localhost:1337/api/upload', imgData ,{
         headers:{
-            Authorization:`Bearer ${userData.jwt}`
+            Authorization: `Bearer ${userData.jwt}`
         }
     })
+    .then(response =>{
+        bookObject.cover = response.data[0].id
+        axios.post(`http://localhost:1337/api/${radioValue}`, {
+            data:bookObject
+        },
+        {
+            headers:{
+                Authorization:`Bearer ${userData.jwt}`
+            }
+        })
+    })
+    .then(response =>{
+        console.log(response)
+    })
+
 })
 
 async function initFormula(){
@@ -61,15 +99,95 @@ async function initFormula(){
     console.log(today.toISOString().slice(0, 10))
     // $('#input-field-date').val(new Date().toDateInputValue());
 
+
+    //genre option dropdown
     const genreOptions = $('#genre-option');
 
     await axios.get('http://localhost:1337/api/genres')
     .then(response =>{
         const {data:{data}} = response;
         data.forEach(g =>{
-            genreOptions.append(`<option value="${g.id}">${g.attributes.name}</option>`)
+            const option = $(`<option value="${g.id}">${g.attributes.name}</option>`)
+            genreOptions.append(option)
+        })
+
+    })
+    
+    genreOptions.on('change', (e)=>{
+        const genre = e.target.children[e.target.value-1]
+
+        if($(`span.option-value[value=${genre.value}]`).length >= 1){
+            return;
+        }
+
+        const option = $(`<span class="option-value">${genre.textContent}</span>`);
+        option.val(genre.value)
+
+        genreValues.append(option)
+
+        option.on('click', (e)=>{
+            option.remove();
         })
     })
+
+    //add behaviour for multiple value inputs
+    addMultipleValueBehavior($('#add-author-btn') ,authorInputfield, authorsValues)
+    addMultipleValueBehavior($('#add-narrator-btn') ,narrationsInputfield, narrationsValues)
+
 }
 
 initFormula();
+
+// $('#add-author-btn').on('click', ()=>{
+//     const value = authorInputfield.val();
+//     authorInputfield.val("");
+
+//     if(value == ""){
+//         return;
+//     }
+
+//     const option = $(`<span class="option-value" value="${value}">${value}</span>`);
+//     authorsValues.append(option);
+//     option.on('click', (e)=>{
+//         option.remove();
+//     })
+
+// })
+
+// $('#add-narrator-btn').on('click', ()=>{
+//     const value = narrationsInputfield.val();
+//     narrationsInputfield.val("");
+
+//     if(value == ""){
+//         return;
+//     }
+
+//     const option = $(`<span class="option-value" value="${value}">${value}</span>`);
+//     narrationsValues.append(option);
+//     option.on('click', (e)=>{
+//         option.remove();
+//     })
+// })
+
+function addMultipleValueBehavior(button, inputfield, multipleValueContainer){
+    console.log(inputfield, multipleValueContainer)
+    button.on('click', ()=>{
+        const value = inputfield.val();
+
+        if(multipleValueContainer.children(`[value="${value}"]`).length >= 1){
+            return;
+        }
+        console.log('add')
+        inputfield.val("");
+    
+        if(value == ""){
+            return;
+        }
+    
+        const option = $(`<span class="option-value" value="${value}">${value}</span>`);
+        multipleValueContainer.append(option);
+        option.on('click', (e)=>{
+            option.remove();
+        })
+    })
+}
