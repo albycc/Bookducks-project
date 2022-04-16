385e61c2841c46bb589912eab61180b485786af4
albycc <78078184+albycc@users.noreply.github.com>
2022-04-16 11:53:46 +0200

    disabled login button when loging in
    if failed login, the button will be enabled again
    
    added form style from style.css to login page

--- frontend/pages/logincreateaccount/js/login.js
+++ frontend/pages/logincreateaccount/js/login.js
@@ -3,16 +3,28 @@ const emailField = $('#email-input-field');
 const passwordField = $('#password-input-field');
 const errorMessage = $('#error-message');
 
-$('#login-btn').on('click', (e)=>{
-    e.preventDefault();
 
+const loginbutton = $('#login-btn')
+
+loginbutton.on('click', (e)=>{
+    login(e);
+})
+
+$('#login-form').on('submit', (e)=>{
+    login(e)
+})
+
+async function login(e){
+    e.preventDefault();
+    
     if(emailField.val() == "" || passwordField.val() == ""){
         printErrorMessage("Empty fields");
         return;
     }
-
+    
+    loginbutton.attr('disabled', true)
     console.log('login function call')
-    axios.post('http://localhost:1337/api/auth/local', {
+    await axios.post('http://localhost:1337/api/auth/local', {
         identifier: emailField.val(),
         password: passwordField.val(),
     })
@@ -22,14 +34,17 @@ $('#login-btn').on('click', (e)=>{
         sessionStorage.setItem('userData', JSON.stringify(data))
         console.log('Login details: ', data);
         location.href= '../../index.html';
-
+    
     })
     .catch(error =>{
+        console.log(error.response)
         if(error.response.status == 400){
             printErrorMessage('Wrong email or password')
+            loginbutton.removeAttr('disabled')
+    
         }
     })
-})
+}
 
 export function printErrorMessage(message){
     if(errorMessage.parent().hasClass('hide-visible')){
