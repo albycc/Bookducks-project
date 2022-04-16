e59dd5cf5a597a40213fc0ae9dd89f4c2f3b1b1b
albycc <78078184+albycc@users.noreply.github.com>
2022-04-16 14:00:02 +0200

    Create account validation
    error message will appear if fields are empty, email or username is taken, email is not valid or the user hasn't ticked the agreement checkbox
    
    a popup window will appear informing the user's sussecsful account creation

--- frontend/pages/logincreateaccount/js/createaccount.js
+++ frontend/pages/logincreateaccount/js/createaccount.js
@@ -1,33 +1,73 @@
-// createaccount.js
-// import {printErrorMessage} from './login.js'
 
 const usernameInputfield = $('#username-input-field');
-const passwordInputfield = $('#password-input-field');
 const emailInputfield = $('#email-input-field');
+const passwordInputfield = $('#password-input-field');
 
-$('#login-btn').on('click', async (e)=>{
+const errorMessage = $('#error-message')
+
+const createBtn = $('#create-account-btn');
+
+createBtn.on('click', async (e)=>{
     e.preventDefault();
 
-    console.log(usernameInputfield.val(), passwordInputfield.val(), emailInputfield.val())
+    errorMessage.parent().addClass('hide-visible')
 
-    if(usernameInputfield.val() == "" || emailInputfield.val() == "" || passwordInputfield.val() == ""){
-        console.log("Empty fields");
+    if(!$('#terms-checkbox').is(':checked')){
+        printErrorMessage('Agree to the terms.')
         return;
-
     }
 
-    const response = await axios.post('http://localhost:1337/api/auth/local/register', {
+    const requriedFields = $('[required]').filter(function(){
+        return this.value == "";
+    })
+
+
+    if(requriedFields.length >= 1){
+        console.log('empty fields');
+        printErrorMessage('Missing fields')
+        return;
+    }
+
+    await axios.post('http://localhost:1337/api/auth/local/register', {
         username:usernameInputfield.val(),
         password:passwordInputfield.val(),
         email:emailInputfield.val(),
     })
-    
+    .then(response =>{
+        console.log(response);
+        if(response.status==200){
 
-    console.log('created account function')
+            const popupMessage = $('<div class="message-box"></div>');
+
+            popupMessage.append(`
+                <div class="popup-container">
+                    <div class="message-box">
+                        <h2>Account created succesful!</h2>
+                        <p>Now enjoy plenty of books <br> here at</p>
+                        <p>Book ducks</p>
+                        <a href="../../index.html">Search books<a>
+                        <button id="close-popup-btn">Close</button>
+                    </div>
+                </div>
+            `);
+
+            $('.login-page-layout').before(popupMessage);
+
+            $('#close-popup-btn').on('click', ()=>{
+                sessionStorage.setItem('userData', JSON.stringify(response.data))
+                location.href= '../../index.html';
+            })
+        }
+    })
+    .catch(error =>{
+        console.log(error.response);
+        printErrorMessage(error.response.data.error.message)
+    })
 });
 
-// (function(){
-//     usernameInputfield.val('Robert');
-//     emailInputfield.val('robert@gmail.com');
-//     passwordInputfield.val('test1234')
-// })();
+function printErrorMessage(message){
+    if(errorMessage.parent().hasClass('hide-visible')){
+        errorMessage.parent().removeClass('hide-visible');
+    }
+    errorMessage.text(message)
+}
